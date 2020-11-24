c6735ee774519f4e5ae7f86067cd8866d698b4d2
Jongheon <jongheon@Jongheonui-MacBookPro.local>
2020-11-24 14:25:11 +0900

    fix2

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const apiRouter = express.Router();
 app.use(logger("dev", {}));
 app.use(bodyParser.json());
 app.use("/api", apiRouter);
-
+//
 apiRouter.post("/sayHello", function (req, res) {
   const responseBody = {
     version: "2.0",
@@ -68,49 +68,47 @@ apiRouter.post("/menu", function (req, res) {
   res.status(200).send(responseBody);
 });
 
-
-
-apiRouter.post("/test", function(req, res) {
-    return res.send({
-      version: "2.0",
-      template: {
-        outputs: [
-          {
-            basicCard: {
-              title: "간단한 텍스트와 버튼요소",
-              description: "간단한 버튼과 텍스트입니다",
-              buttons: [
-                {
-                  action: "message",
-                  label: "열어보기"
-                }
-              ]
-            }
-          }
-        ]
-      }
-    });
+apiRouter.post("/test", function (req, res) {
+  return res.send({
+    version: "2.0",
+    template: {
+      outputs: [
+        {
+          basicCard: {
+            title: "간단한 텍스트와 버튼요소",
+            description: "간단한 버튼과 텍스트입니다",
+            buttons: [
+              {
+                action: "message",
+                label: "열어보기",
+              },
+            ],
+          },
+        },
+      ],
+    },
   });
+});
 
-  app.post("/blockId", function(req, res) {
-    const userRequest = req.body.userRequest;
-    const blockId = userRequest.block.id;
-  
-    return res.send({:
-      version: "2.0",
-      template: {
-        outputs: [
-          {
-            basicCard: {
-              title: "블록ID 입니다",
-              description: blockId
-            }
-          }
-        ]
-      }
-    });
-  });
+app.post("/blockId", function (req, res) {
+  const userRequest = req.body.userRequest;
+  const blockId = userRequest.block.id;
 
-  app.listen(3000, function() {
-    console.log('Example skill server listening on port 3000!');
+  return res.send({
+    version: "2.0",
+    template: {
+      outputs: [
+        {
+          basicCard: {
+            title: "블록ID 입니다",
+            description: blockId,
+          },
+        },
+      ],
+    },
   });
+});
+
+app.listen(3000, function () {
+  console.log("Example skill server listening on port 3000!");
+});
