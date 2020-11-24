9a1db94458db9de8a14f75fae7f232bd65ec4ffc
supreme034@korea.ac.kr <supreme034@korea.ac.kr>
2020-11-24 23:42:03 +0900

    fix1.6

--- index.js
+++ index.js
@@ -120,6 +120,33 @@ apiRouter.post("/searchBook", function (req, res) {
   });
 });
 
+apiRouter.post("/reserveSeat", function (req, res) {
+  return res.send({
+    version: "2.0",
+    template: {
+      outputs: [
+        {
+          basicCard: {
+            title: "학술정보원 열람실 예약",
+            description: "팝업창 로그인후 사용해주세요!",
+            thumbnail: {
+              imageUrl:
+                "https://lh3.googleusercontent.com/proxy/IO0N4d6B0rjRKjBPCYBSTnI09BojxaXw9R8IXGyo6ClnG9Zs3NDUUmWvdjuSx1AbjxDc4NBiaTqqR4SHLFYIo0cZHjL2DMscj1KmO51DldPBVntokqieuq5Rk3uUPw4iMtSu_QkVQ7o-Km5quzGh4qEFMQoH_7nT_qSsmmWeUliKwMuOc8DIUJgQ15JW5JNoyvuvK5_-Ib1WH4ETtjjmL5qjDtbzHbiT4ihoUuiR1_ZheVphEpAnnh2w7WklCM8dISFZDYwuDXfQZevUGgulWNRzj_cXt5zUl4MOanfFzCOqIUR8TxzQytVqlOk2WoYCaaP6ocwfIAKEK6NivqgwuMuZtg0OVyKHZhddN0bTC1lkkphWIP_9eGlFUA7Y5w",
+            },
+            buttons: [
+              {
+                action: "webLink",
+                label: "Link Button",
+                webLinkUrl: "https://libs.korea.ac.kr:81/",
+              },
+            ],
+          },
+        },
+      ],
+    },
+  });
+});
+
 apiRouter.post("/location", function (req, res) {
   const userRequest = req.body.userRequest;
   const blockId = userRequest.block.id;
@@ -191,27 +218,6 @@ apiRouter.post("/restaurant", function (req, res) {
 app.post("/blockId", function (req, res) {
   const userRequest = req.body.userRequest;
   const blockId = userRequest.block.id;
-
-  return res.send({
-    version: "2.0",
-    template: {
-      outputs: [
-        {
-          basicCard: {
-            title: "블록ID 입니다",
-            description: blockId,
-          },
-        },
-      ],
-    },
-  });
-});
-
-
-app.post("/blockId", function (req, res) {
-  const userRequest = req.body.userRequest;
-  const blockId = userRequest.block.id;
-  const userTime = params.time;  
 
   return res.send({
     version: "2.0",
