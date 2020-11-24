d072759858ff8e1628db95edf6f6ea0992e8f66f
Jongheon <jongheon@Jongheonui-MacBookPro.local>
2020-11-24 13:51:17 +0900

    first commit

--- index.js
+++ index.js
@@ -47,6 +47,70 @@ apiRouter.post("/showHello", function (req, res) {
   res.status(200).send(responseBody);
 });
 
-app.listen(3000, function() {
+apiRouter.post("/menu", function (req, res) {
+  console.log(req.body);
+
+  const responseBody = {
+    version: "2.0",
+    template: {
+      outputs: [
+        {
+          simpleImage: {
+            imageUrl:
+              "https://sejong.korea.ac.kr/dext5editordata/20201116_112917917_91726.jpg",
+            altText: "hello I'm Ryan",
+          },
+        },
+      ],
+    },
+  };
+
+  res.status(200).send(responseBody);
+});
+
+
+
+apiRouter.post("/test", function(req, res) {
+    return res.send({
+      version: "2.0",
+      template: {
+        outputs: [
+          {
+            basicCard: {
+              title: "간단한 텍스트와 버튼요소",
+              description: "간단한 버튼과 텍스트입니다",
+              buttons: [
+                {
+                  action: "message",
+                  label: "열어보기"
+                }
+              ]
+            }
+          }
+        ]
+      }
+    });
+  });
+
+  app.post("/blockId", function(req, res) {
+    const userRequest = req.body.userRequest;
+    const blockId = userRequest.block.id;
+  
+    return res.send({:
+      version: "2.0",
+      template: {
+        outputs: [
+          {
+            basicCard: {
+              title: "블록ID 입니다",
+              description: blockId
+            }
+          }
+        ]
+      }
+    });
+  });
+
+  app.listen(3000, function() {
     console.log('Example skill server listening on port 3000!');
   });
