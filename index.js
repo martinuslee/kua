e0c4ec8a7a1d8822617d6e8bae5eceb0dcb69adf
supreme034@korea.ac.kr <supreme034@korea.ac.kr>
2020-11-24 19:26:03 +0900

    fix1.6

--- index.js
+++ index.js
@@ -68,12 +68,22 @@ apiRouter.post("/diagnosis", function (req, res) {
           basicCard: {
             title: "코로나-19 자가진단",
             description: "COVID-19 Self-diagnosis",
+            thumbnail:{
+              imageUrl:"https://pusyap.com/wp-content/uploads/2020/07/%EC%B2%B4%EC%98%A8%EC%B8%A1%EC%A0%95_%EC%BD%94%EB%A1%9C%EB%82%9819jpg.jpg"
+            },
             buttons: [
               {
                 action: "webLink",
-                label: "Link Button",
+                label: "구글 링크 바로가기(Google from Link)",
                 webLinkUrl:
                   "https://docs.google.com/forms/d/e/1FAIpQLSf0oC7eK8KmBLXJfITgk7ZJN-aB2jUcN6aBUcaLNhgpJQGYlw/viewform",
+              },
+            ],
+            buttons: [
+              {
+                action: "phone",
+                label: "선별진료소 전화하기(Call Emergence)",
+                phoneNumber: "044-860-1038"
               },
             ],
           },
