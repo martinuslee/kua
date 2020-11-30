be3e1d99c139dd40e349fc1421151db3bfab8ed2
supreme034@korea.ac.kr <supreme034@korea.ac.kr>
2020-12-01 03:25:35 +0900

    fix

--- router/location.js
+++ router/location.js
@@ -14,7 +14,9 @@ let library = "학술정보원";
 let secondST = "제2과학기술대학";
 let firstST = "제1과학기술대학";
 let cultureSport = "문화스포츠관";
-
+let policy = "공공정책대학";
+let pharm = "약학대학 연구실험동";
+let clutser = "산학협력관";
 let maplink;
 
 //http://3.35.56.248:3000/api/location
@@ -30,29 +32,31 @@ apiRouter.post("/location", function (req, res) {
 
   switch (where) {
     case nongshim:
-      maplink =
-        "https://map.kakao.com/link/map/농심국제관,36.60918555652231,127.28552189796417";
+      maplink = "/농심국제관,36.60918555652231,127.28552189796417";
       break;
     case library:
-      maplink =
-        "https://map.kakao.com/link/map/학술정보원,36.61004854502758,127.28714058017081";
+      maplink = "/학술정보원,36.61004854502758,127.28714058017081";
       break;
     case secondST:
-      maplink =
-        "https://map.kakao.com/link/map/제2과학기술대학,36.61104430370841, 127.28688799534845";
+      maplink = "/제2과학기술대학,36.61104430370841, 127.28688799534845";
       break;
     case firstST:
-      maplink =
-        "https://map.kakao.com/link/map/제1과학기술대학,36.60999264660403, 127.2845548753664";
+      maplink = "/제1과학기술대학,36.60999264660403, 127.2845548753664";
       break;
     case cultureSport:
-      maplink =
-        "https://map.kakao.com/link/map/문화스포츠관,36.61123638564794, 127.29073465209953";
+      maplink = "/문화스포츠관,36.61123638564794, 127.29073465209953";
+      break;
+    case policy:
+      maplink = "/공공정책대학,36.61103445686396, 127.28690752487559";
+      break;
+    case pharm:
+      maplink = "/약학대학 연구실험동,36.6094981593413, 127.28345650645907";
+      break;
+    case clutser:
+      maplink = "/산학협력관,36.609097689308975, 127.28434163543477";
       break;
     default:
-      maplink = "https://map.kakao.com";
-      where ="목적지"
-      mapName = "default";
+      maplink = "/";
       break;
   }
 
@@ -72,7 +76,7 @@ apiRouter.post("/location", function (req, res) {
               {
                 action: "webLink",
                 label: "클릭해서 바로 길 찾기",
-                webLinkUrl: maplink,
+                webLinkUrl: "https://map.kakao.com/link/map/" + maplink,
               },
             ],
           },
