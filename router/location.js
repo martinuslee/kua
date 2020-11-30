98236eb9bce0c58ca32a8b82e41d1ca8b6e4a5fd
supreme034@korea.ac.kr <supreme034@korea.ac.kr>
2020-12-01 02:54:12 +0900

    fix

--- router/location.js
+++ router/location.js
@@ -13,6 +13,7 @@ let nongshim = "농심국제관";
 let library = "학술정보원";
 let secondST = "제2과학기술대학";
 let firstST = "제1과학기술대학";
+let cultureSport = "문화스포츠관";
 
 let maplink;
 
@@ -21,8 +22,8 @@ apiRouter.post("/location", function (req, res) {
   const question = req.body.userRequest.utterance; // 입력 발화
 
   console.log(question);
-  const Action = req.body.action;
-  const where = Action.params.sys_building;
+  let Action = req.body.action;
+  let where = Action.params.sys_building;
   // console.log(typeof(where));
   console.log(Action.params);
   console.log(where);
@@ -31,25 +32,26 @@ apiRouter.post("/location", function (req, res) {
     case nongshim:
       maplink =
         "https://map.kakao.com/link/map/농심국제관,36.60918555652231,127.28552189796417";
-      mapName = nongshim;
       break;
     case library:
       maplink =
         "https://map.kakao.com/link/map/학술정보원,36.61004854502758,127.28714058017081";
-      mapName = library;
       break;
     case secondST:
       maplink =
-        "https://map.kakao.com/link/map/제2과학기술대학,36.60918555652231,127.28552189796417";
-      mapName = secondST;
+        "https://map.kakao.com/link/map/제2과학기술대학,36.61104430370841, 127.28688799534845";
       break;
     case firstST:
       maplink =
-        "https://map.kakao.com/link/map/제1과학기술대학,36.61004854502758,127.28714058017081";
-      mapName = firstST;
+        "https://map.kakao.com/link/map/제1과학기술대학,36.60999264660403, 127.2845548753664";
+      break;
+    case cultureSport:
+      maplink =
+        "https://map.kakao.com/link/map/문화스포츠관,36.61123638564794, 127.29073465209953";
       break;
     default:
       maplink = "https://map.kakao.com";
+      where ="목적지"
       mapName = "default";
       break;
   }
