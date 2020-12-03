6f9f540658be3d1a55f1a0c03e03aa56b0c930eb
supreme034@korea.ac.kr <supreme034@korea.ac.kr>
2020-12-03 19:40:41 +0900

    index //

--- index.js
+++ index.js
@@ -15,6 +15,13 @@ const announceList = require('./router/announce');
 
 app.use(logger("dev", {}));
 app.use(bodyParser.json());
+//post로 요청된 json 타입 body를 쉽게 추출할 수 있는 모듈.
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+// extended 는 중첩된 객체표현을 허용할지 말지를 정하는 것이다. 객체 안에 객체를 파싱할 수 있게하려면 true.
+
+
 
 //각 기능을 미들웨어로 인테그레이션
 app.use("/api", userMenu);
