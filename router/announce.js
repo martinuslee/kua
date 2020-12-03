41627dbd6c344a258dd85a09b50c58758f33ac64
supreme034@korea.ac.kr <supreme034@korea.ac.kr>
2020-12-03 19:10:36 +0900

    name modified

--- router/announce.js
+++ router/announce.js
@@ -15,8 +15,6 @@ apiRouter.post("/announce", function (req, res) {
               {
                 title: "학사일반공지",
                 description: "학사일반공지 바로가기",
-                imageUrl:
-                  "http://k.kakaocdn.net/dn/APR96/btqqH7zLanY/kD5mIPX7TdD2NAxgP29cC0/1x1.jpg",
                 link: {
                   web: "https://sejong.korea.ac.kr/campuslife/notice/college",
                 },
@@ -24,8 +22,6 @@ apiRouter.post("/announce", function (req, res) {
               {
                 title: "장학공지",
                 description: "교내, 교외 장학금 공지 바로가기",
-                imageUrl:
-                  "http://k.kakaocdn.net/dn/N4Epz/btqqHCfF5II/a3kMRckYml1NLPEo7nqTmK/1x1.jpg",
                 link: {
                   web:
                     "http://sejong.korea.ac.kr/campuslife/notice/scholarship",
@@ -34,8 +30,6 @@ apiRouter.post("/announce", function (req, res) {
               {
                 title: "학사일정",
                 description: "학기 일정표 바로가기",
-                imageUrl:
-                  "http://k.kakaocdn.net/dn/bE8AKO/btqqFHI6vDQ/mWZGNbLIOlTv3oVF1gzXKK/1x1.jpg",
                 link: {
                   web:
                     "https://sejong.korea.ac.kr/academics/administration/calendar/undergraduate",
