3e02208e8d3919640ab7da27d990a97c5aae73f7
supreme034@korea.ac.kr <supreme034@korea.ac.kr>
2021-03-09 22:48:51 +0900

    cafeteria update

--- router/menu.js
+++ router/menu.js
@@ -1,32 +1,49 @@
 const express = require("express");
 const apiRouter = express.Router();
+const axios = require("axios"); //html로 웹자료를 get
+const cheerio = require("cheerio"); //html을 JS로 변환
 
-apiRouter.post("/menu", function (req, res) {
-    console.log(req.body);
-  
-    const responseBody = {
-      version: "2.0",
-      template: {
-        outputs: [
-          {
-            basicCard: {
-              title: "금주의 교직원 식당 식단표입니다",
-              description: "웹사이트에서 교직원식당 주간 메뉴를 클릭해주세요!",
-              buttons: [
-                {
-                  action: "webLink",
-                  label: "Link Button",
-                  webLinkUrl:
-                    "http://portal.korea.ac.kr/common/Download.kpd?filePath=/files/groupware/upload/202101/202101041028070725_%EA%B5%90%EC%A7%81%EC%9B%90%EC%8B%9D%EB%8B%B9%20%EC%A3%BC%EA%B0%84%20%EB%A9%94%EB%89%B4.pdf&fileName=%EA%B5%90%EC%A7%81%EC%9B%90%EC%8B%9D%EB%8B%B9%20%EC%A3%BC%EA%B0%84%20%EB%A9%94%EB%89%B4.pdf",
-                },
-              ],
-            },
-          },
-        ],
-      },
-    };
-  
-    res.status(200).send(responseBody);
+let $href = [];
+axios
+  .get(`https://sejong.korea.ac.kr/campuslife/facilities/dining/weeklymenu`)
+  .then((response) => {
+    const $ = cheerio.load(response.data);
+    // $(".buttonGo floatR").each((index, item) => {
+    //    menulink = item.attribs.href;
+    // });
+    links = $("a");
+    $(links).each((i, link) => {
+      //console.log(i + ' : ' + $(link).text() +': '+$(link).attr('href')+ '\n');
+      if($(link).text() === '교직원식당 주간 메뉴') $href.push($(link).attr('href'));
+    });
+    console.log('https://sejong.korea.ac.kr' + $href);
   });
 
-  module.exports = apiRouter;
+apiRouter.post("/menu", function (req, res) {
+  console.log(req.body);
+
+  const responseBody = {
+    version: "2.0",
+    template: {
+      outputs: [
+        {
+          basicCard: {
+            title: "금주의 교직원 식당 식단표입니다",
+            description: "웹사이트에서 교직원식당 주간 메뉴를 클릭해주세요!",
+            buttons: [
+              {
+                action: "webLink",
+                label: "Link Button",
+                webLinkUrl: 'https://sejong.korea.ac.kr' + $href,
+              },
+            ],
+          },
+        },
+      ],
+    },
+  };
+
+  res.status(200).send(responseBody);
+});
+
+module.exports = apiRouter;
