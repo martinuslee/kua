92e53a5626bf435ca14d9eb110a3c948bd9d1ccf
supreme034@korea.ac.kr <supreme034@korea.ac.kr>
2021-03-16 00:56:36 +0900

    annotation menu

--- router/menu.js
+++ router/menu.js
@@ -2,23 +2,22 @@ const express = require("express");
 const apiRouter = express.Router();
 const axios = require("axios"); //html로 웹자료를 get
 const cheerio = require("cheerio"); //html을 JS로 변환
+
 let $href = [];
 
 apiRouter.post("/menu", function (req, res) {
-  console.log(req.body);
+  //console.log(req.body);
 
   axios
     .get(`https://sejong.korea.ac.kr/campuslife/facilities/dining/weeklymenu`)
     .then((response) => {
       const $ = cheerio.load(response.data);
-      // $(".buttonGo floatR").each((index, item) => {
-      //    menulink = item.attribs.href;
-      // });
-      links = $("a");
+
+      links = $("a"); //<a> 태그 전부 가져옴
       $(links).each((i, link) => {
         //console.log(i + ' : ' + $(link).text() +': '+$(link).attr('href')+ '\n');
-        if ($(link).text() === "교직원식당 주간 메뉴")
-          $href=($(link).attr("href"));
+        if ($(link).text() === "교직원식당 주간 메뉴") //a 태그에 텍스트가 교직원식당 주간 메뉴인 부분 찾기
+          $href=($(link).attr("href")); // 찾았으면 href 속성 링크 가져옴
       });
       console.log("https://sejong.korea.ac.kr" + $href);
     });
