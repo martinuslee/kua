78111f12bdf9724aca9b88f55db959bdf4ec8712
supreme034@korea.ac.kr <supreme034@korea.ac.kr>
2021-03-16 00:48:56 +0900

    menu modified

--- router/menu.js
+++ router/menu.js
@@ -3,25 +3,25 @@ const apiRouter = express.Router();
 const axios = require("axios"); //html로 웹자료를 get
 const cheerio = require("cheerio"); //html을 JS로 변환
 
-let $href = [];
-axios
-  .get(`https://sejong.korea.ac.kr/campuslife/facilities/dining/weeklymenu`)
-  .then((response) => {
-    const $ = cheerio.load(response.data);
-    // $(".buttonGo floatR").each((index, item) => {
-    //    menulink = item.attribs.href;
-    // });
-    links = $("a");
-    $(links).each((i, link) => {
-      //console.log(i + ' : ' + $(link).text() +': '+$(link).attr('href')+ '\n');
-      if ($(link).text() === "교직원식당 주간 메뉴")
-        $href.push($(link).attr("href"));
-    });
-    console.log("https://sejong.korea.ac.kr" + $href);
-  });
-
 apiRouter.post("/menu", function (req, res) {
   console.log(req.body);
+
+  let $href = [];
+  axios
+    .get(`https://sejong.korea.ac.kr/campuslife/facilities/dining/weeklymenu`)
+    .then((response) => {
+      const $ = cheerio.load(response.data);
+      // $(".buttonGo floatR").each((index, item) => {
+      //    menulink = item.attribs.href;
+      // });
+      links = $("a");
+      $(links).each((i, link) => {
+        //console.log(i + ' : ' + $(link).text() +': '+$(link).attr('href')+ '\n');
+        if ($(link).text() === "교직원식당 주간 메뉴")
+          $href.push($(link).attr("href"));
+      });
+      console.log("https://sejong.korea.ac.kr" + $href);
+    });
 
   const responseBody = {
     version: "2.0",
