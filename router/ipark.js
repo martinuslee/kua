a1338daec6d8c6b17c59214c76037548d6ee4627
martinuslee <supreme034@korea.ac.kr>
2021-10-05 11:46:56 +0000

    ipark

---
+++ router/ipark.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const apiRouter = express.Router();
+
+const axios = require("axios"); //html로 웹자료를 get
+
+let members = "";
+apiRouter.post("/ipark", function (req, res) {
+  const liveData = "http://cxz3619.pythonanywhere.com/liveData";
+  axios.get(liveData).then((response) => {
+    // console.log(response);
+    const {data} = response;
+    members = data.length;
+    console.log(members);
+  });
+  return res.send({
+    version: "2.0",
+    template: {
+      outputs: [
+        {
+          //  어디인지 모르겠다는 메세지를 반환
+          simpleText: {
+            text: "현재 아이파크에는 "+ members +"명의 회원분들이 운동을 하고 있습니다.",
+          },
+        },
+      ],
+    },
+  });
+});
+
+module.exports = apiRouter;
--- router/searchBook.js
+++ router/searchBook.js
@@ -2,31 +2,30 @@ const express = require("express");
 const apiRouter = express.Router();
 
 apiRouter.post("/searchBook", function (req, res) {
-    return res.send({
-      version: "2.0",
-      template: {
-        outputs: [
-          {
-            basicCard: {
-              title: "학술정보원",
-              description: "아래 버튼을 통해 도서검색, 이용안내 바로가기",
-              thumbnail: {
-                imageUrl:
+  return res.send({
+    version: "2.0",
+    template: {
+      outputs: [
+        {
+          basicCard: {
+            title: "학술정보원",
+            description: "아래 버튼을 통해 도서검색, 이용안내 바로가기",
+            thumbnail: {
+              imageUrl:
                 "https://www.inews365.com/data/photos/20190940/art_1569820010941.jpg",
-              },
-              buttons: [
-                {
-                  action: "webLink",
-                  label: "Link Button",
-                  webLinkUrl: "https://libs.korea.ac.kr/", //도서관 웹페이지 링크 반환
-                },
-              ],
             },
+            buttons: [
+              {
+                action: "webLink",
+                label: "Link Button",
+                webLinkUrl: "https://libs.korea.ac.kr/", //도서관 웹페이지 링크 반환
+              },
+            ],
           },
-        ],
-      },
-    });
+        },
+      ],
+    },
   });
+});
 
-  module.exports = apiRouter;
-  
+module.exports = apiRouter;
